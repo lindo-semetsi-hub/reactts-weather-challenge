a362a1ca391c4a0164cdb95610bf5b3fab7aaf9b
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-25 02:40:27 +0200

    commit current changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route, NavLink} from 'react-router-dom';
+import { useState } from "react";
+
+import Settings from './pages/Settings';
+import Home from "./pages/Home.tsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+ 
+ const toggleTheme = () => {
+  setTheme(theme === "light" ? "dark" : "light");
+ };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+    <div data-theme={theme}>
+      {/* --- header/nav section --- */}
+      <header style={{ padding: "1rem", borderBottom: "1px solid #ccc"}}>
+{/* navLink is nice because it highlights when active */}
+          <NavLink to="/" style={{marginRight: "1rem"}}>Home</NavLink>
+          <NavLink to ="/settings" className="link">Settings</NavLink>
+       
+       {/* Theme Button */}
+        <button onClick={toggleTheme} style={{marginLeft: "1rem"}}>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        </header>
+
+        {/* --- main content --- */}
+        <main style={{padding: "1rem"}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/settings" element = {<Settings />} />
+          </Routes>
+        </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <h1> Weather App</h1>
+            <p> This is where the current weather will show soon...</p>
+        </div>
+    );
+}
