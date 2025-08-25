81ece2ad82599007f5e276276dd6c1b59b6bd530
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-08-25 02:42:53 +0200

    commit settings placeholder

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+    return (
+        <div>
+            <h1> Settings</h1>
+            <p> Theme and units will be configurable here. </p>
+        </div>
+    );
+}
